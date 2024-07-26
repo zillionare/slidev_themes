f11da7f78bda90e385cbd7032f2bdb8e0bf0b4f7
aaron yang <aaron_yang@jieyu.ai>
2024-07-26 10:51:25 +0800

    ✨ feat: 增加一个radiowave组件

--- components/anime/RadioWave.vue
+++ components/anime/RadioWave.vue
@@ -3,6 +3,17 @@
 电波动画。两个在8点钟方向相切的圆，向右上角缩放。可用在list item前面的圆点展开
 
 -->
+<script setup>
+const colors = {
+    "yellow": ["#ffc107", "#ffc107"],
+    "blue": ["#0d6efd", "#0d6efd"],
+    "green": ["#F1FFCC", "#F8FFE3"],
+    "red": ["#19875420", "#dc354580"],
+    "pink": ["#FFC3DB20", "##FFC3DB20"],
+    "white": ["#ffffffa0", "#fcfcfca0"]
+}
+</script>
+
 <style scoped>
 .circles-container {
     position: relative;
@@ -18,11 +29,13 @@
     height: 100%;
     border-radius: 50%;
     background-color: var(--small-circle-color);
-    animation: scaleAnimation 5s infinite;
+    animation: scaleSmall 5s infinite;
+    animation-delay: 1s;
     transform-origin: 30% 100%;
+    box-shadow: -2px -5px 50px rgba(0, 0, 0, 0.1);
 }
 
-.large-circle {
+.big-circle {
     position: absolute;
     top: -25%;
     /* 调整top值使大圆向上偏移 */
@@ -32,12 +45,12 @@
     height: 130%;
     border-radius: 50%;
     background-color: var(--large-circle-color);
-    animation: scaleAnimation 5s infinite;
+    animation: scaleBig 5s infinite;
     transform-origin: 30% 100%;
+    box-shadow: -2px -5px 100px rgba(0, 0, 0, 0.1);
 }
 
-/* 定义动画 */
-@keyframes scaleAnimation {
+@keyframes scaleBig {
 
     0%,
     100% {
@@ -48,12 +61,24 @@
         transform: scale(1.2);
     }
 }
+
+@keyframes scaleSmall {
+
+    0%,
+    100% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.1);
+    }
+}
 </style>
 
 <template>
     <div class="circles-container"
-        :style="{ '--small-circle-color': $attrs.smallCircle, '--large-circle-color': $attrs.bigCircle }">
+        :style="{ '--small-circle-color': colors[$attrs.color][0], '--large-circle-color': colors[$attrs.color][1] }">
         <div class="small-circle"></div>
-        <div class="large-circle"></div>
+        <div class="big-circle"></div>
     </div>
 </template>
